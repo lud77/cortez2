1f70ec8531c640b05f9be3acc4eb4db4cdc6818e
Stefano Butera <stefano.butera@trinitymirror.com>
2019-06-04 17:10:51 +0100

    Add directed graph data structure

---
+++ src/graph.js
@@ -0,0 +1,58 @@
+const sequence = require('./sequence');
+
+module.exports = () => {
+    const nodes = {};
+    const edges = {};
+
+    const nodeSeq = sequence();
+
+    const inbound = {};
+    const outbound = {};
+
+    const setNode = (id, payload) => {
+        nodes[id] = { id, payload };
+    };
+
+    const addNode = (payload) => {
+        const id = nodeSeq.next();
+        setNode(id, payload);
+
+        return id;
+    };
+
+    const setEdge = (id, sourceId, targetId, payload) => {
+        inbound[targetId] = inbound[targetId] || new Set();
+        inbound[targetId].add(id);
+
+        outbound[sourceId] = outbound[sourceId] || new Set();
+        outbound[sourceId].add(id);
+
+        edges[id] = {
+          id,
+          sorceId,
+          targetId,
+          payload
+        };
+    };
+
+    const addEdge = (sourceId, targetId, payload) => {
+        const id = edgeSeq.next();
+        setEdge(id, sourceId, targetId, payload);
+
+        return id;
+    };
+
+    const getEdgesTo = (nodeId) => inbound[nodeId];
+    const getEdgesFrom = (nodeId) => outbound[nodeId];
+
+    return {
+        nodes,
+        edges,
+        addNode,
+        addEdge,
+        setNode,
+        setEdge,
+        getEdgesTo,
+        getEdgesFrom
+    }
+};
